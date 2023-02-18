a0f81253213e42b85d02b73260b39c5b1185c59e
BRYAN HODGINS <eg0547@wayne.edu>
2023-02-18 12:33:12 -0500

    Server working, mongodb untested

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 const express = require('express');
-require('dotenv').config;
-console.log(process.env)
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const uri = process.env.MONGO_CONNECTION;
+const mongoOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1
+};
+
+//create mongodb client with options from above
+const client = new MongoClient(uri, mongoOptions);
+client.connect;
+
+app.post('/LoginVerify', (req,res) => {    
+    res.send('Successful Login')
+})
+
+app.listen(3000, () => console.log('Example app is listening on port 3000.'));
