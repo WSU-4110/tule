ad4f9154fc6459c614c694f16b7dd9cb9770bfb8
BRYAN HODGINS <eg0547@wayne.edu>
2023-02-18 14:52:37 -0500

    database connection working

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
+//All requests made to this server must have body in json format!!!
 app.use(express.json());
 const uri = process.env.MONGO_CONNECTION;
+console.log(uri);
 const mongoOptions = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -12,11 +14,20 @@ const mongoOptions = {
 };
 
 //create mongodb client with options from above
-const client = new MongoClient(uri, mongoOptions);
-client.connect;
+let client = new MongoClient(uri, mongoOptions);
+
+client.connect();
+
+
+const db = client.db('Tule');
+const usersCollection = db.collection('Users')
+const tasksCollection = db.collection('Tasks')
 
 app.post('/LoginVerify', (req,res) => {
     console.log(req.body);
+
+    const userInfo = usersCollection.find({Username: req.body['Username']}).toArray()
+    console.log(userInfo)
     res.send('Successful Login');
 })
 
