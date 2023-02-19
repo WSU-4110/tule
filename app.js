2cd91b83a035a575bb86d8cb0380d392982b6b05
BRYAN HODGINS <eg0547@wayne.edu>
2023-02-19 16:56:25 -0500

    Login Route Working

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
-//All requests made to this server must have body in json format!!!
+//!!!All requests made to this server must have body in json format!!!
 app.use(express.json());
 const uri = process.env.MONGO_CONNECTION;
 console.log(uri);
@@ -24,11 +24,22 @@ const usersCollection = db.collection('Users')
 const tasksCollection = db.collection('Tasks')
 
 app.post('/LoginVerify', (req,res) => {
-    console.log(req.body);
-
-    const userInfo = usersCollection.find({Username: req.body['Username']}).toArray()
-    console.log(userInfo)
-    res.send('Successful Login');
-})
+    usersCollection.find({Username: req.body.Username}).toArray().then(info => {
+        if(info.length == 1){
+            if(info[0]['Password'] === req.body.Password){
+                res.send("Login Success")
+            }
+            else{
+                res.send("Invalid Password")
+            }
+        }
+        else if(info.length > 1){
+            res.send("Warning!! Duplicate usernames exist")
+        }
+        else{
+            res.send("Username not found")
+        }
+    })
+});
 
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
