2fdc5ec740878a74fd2c614405ff5ece3f784a6a
Oskar Schermerhorn <hd5808@wayne.edu>
2023-02-21 22:39:50 -0500

    Created variables and print on submit

--- tule-app/src/Login.jsx
+++ tule-app/src/Login.jsx
@@ -1,14 +1,23 @@
+import React, { useState } from "react"
+
 export const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username);
+        console.log(password);
+    }
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
             <h1>Login Page</h1>
-            <label for="username">username</label>
-            <input for="username"></input>
+            <label htmlFor="username">username</label>
+            <input value={username} onChange={(u) => setUsername(u.target.value)} type="username" placeholder="username"></input>
             <br></br>
-            <label for="password">password</label>
-            <input for="password"></input>
+            <label htmlFor="password">password</label>
+            <input value={password} onChange={(p) => setPassword(p.target.value)} type="password" placeholder="********"></input>
             <br></br>
-            <button for="Submit">Submit</button>
+            <button type="submit">Submit</button>
         </form>
     )
 }
