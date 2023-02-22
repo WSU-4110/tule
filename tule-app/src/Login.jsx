8df62e032cbc48ada6f773150a45ba5d276815e1
Oskar Schermerhorn <hd5808@wayne.edu>
2023-02-21 22:01:45 -0500

    Added password input

--- tule-app/src/Login.jsx
+++ tule-app/src/Login.jsx
@@ -4,6 +4,9 @@ export const Login = () => {
             <h1>Login Page</h1>
             <label for="username">username</label>
             <input for="username"></input>
+            <br></br>
+            <label for="password">password</label>
+            <input for="password"></input>
         </form>
     )
 }
