b4146a0b38df5ec2f720defa792411a108d531bd
Oskar Schermerhorn <hd5808@wayne.edu>
2023-02-28 19:11:02 -0500

    Functionality to switch screens

--- tule-app/src/App.js
+++ tule-app/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import Init from "./Init";
 import Login from "./Login";
@@ -5,11 +6,29 @@ import {SignUp} from "./SignUp";
 
 
 function App() {
+  const [currentScreen, setCurrentScreen] = useState('login');
+  const changeCurrentScreen = (screen) =>{
+    console.log(screen);
+    setCurrentScreen(screen);
+  }
+  const displayScreen = () =>{
+    switch(currentScreen){
+      case 'login':
+        <Login onChangeScreen={changeCurrentScreen}/>
+        break;
+      case 'signup':
+        <SignUp onChangeScreen={changeCurrentScreen}/>
+        break;
+      default:
+        <Init onChangeScreen={changeCurrentScreen}/>
+        break;
+    }
+  }
   return (
     <div className="App">
-      <Init />
-      {/* <Login/> */}
-      {/* <SignUp/> */}
+      {
+        currentScreen === 'signup'? <SignUp onChangeScreen={changeCurrentScreen}/> : <Login onChangeScreen={changeCurrentScreen}/>
+      }
     </div>
   );
 }
