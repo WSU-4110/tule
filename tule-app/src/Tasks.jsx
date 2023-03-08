af12161988e09b7990ce6572c22008134f008ce1
Oskar Schermerhorn <hd5808@wayne.edu>
2023-03-07 19:48:59 -0500

    Update tasks page

--- tule-app/src/Tasks.jsx
+++ tule-app/src/Tasks.jsx
@@ -1,5 +1,21 @@
 import React, { useState } from "react"
 
+const tasklist = [
+    {
+        id: 'task0',
+        taskName: 'Software Engineering class',
+        startTime: '4:00pm',
+        duration: '01:15:00',
+        priority: 'High'
+    },
+    {
+        id: 'task1',
+        taskName: 'Software Engineering homework',
+        startTime: '',
+        duration: '01:30:00',
+        priority: 'Medium'
+    }
+]
 const Tasks = (props) => {
     const handleSubmit = () => {
         console.log("add a task")
@@ -7,10 +23,12 @@ const Tasks = (props) => {
     return(
         <form onSubmit={handleSubmit}>
             <h1>Task list Page</h1>
-            <br></br>
-            <input type={"checkbox"}></input>
-            <label>Task1</label>
-            <br></br>
+            <section>
+                {tasklist.map((task) => {
+                    return <li key={task.id}>{task.taskName}</li>;
+                })
+                }
+            </section>
             <button type="submit">Add task</button>
             <br></br>
             <button onClick={() => props.onChangeScreen('schedule')}>Create Schedule</button>
