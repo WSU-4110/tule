d58620f32c81e917413465edd16ae9c7f88512da
Oskar Schermerhorn <hd5808@wayne.edu>
2023-03-19 20:12:45 -0400

    Design Pattern Builder

--- tule-app/src/Tasks.jsx
+++ tule-app/src/Tasks.jsx
@@ -1,5 +1,5 @@
 import TaskList from './components/TaskList'
-import React from "react"
+import React, { useState } from "react"
 
 const currentTasks = [
     {
@@ -18,25 +18,33 @@ const currentTasks = [
     }
 ]
 const Tasks = (props) => {
+    const [newTaskName, setNewTaskName]= useState('');
+    const [newTaskDuration, setNewTaskDuration]= useState('');
+    const [newTaskPriority, setNewTaskPriority]= useState('');
+
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log("add a task")
         const newTask ={
             id: 'task' + currentTasks.length,
-            taskName: 'Default taskname',
+            taskName: newTaskName,
             startTime: '',
-            duration: '00:00:00',
-            priority: 'None'
+            duration: newTaskDuration,
+            priority: newTaskPriority
         };
         currentTasks.push(newTask);
         console.log(currentTasks);
     }
+
     return(
         <form onSubmit={handleSubmit}>
             <h1>Task list Page</h1>
             <section>
                 <TaskList ListOfTasks={currentTasks} />
             </section>
+            <input value={newTaskName} onChange={(n) => setNewTaskName(n.target.value)} placeholder={'taskName'}></input>
+            <input value={newTaskDuration} onChange={(d) => setNewTaskDuration(d.target.value)} placeholder={'duration'}></input>
+            <input value={newTaskPriority} onChange={(p) => setNewTaskPriority(p.target.value)} placeholder={'priority'}></input>
             <button type="submit">Add task</button>
             <br></br>
             <button onClick={() => props.onChangeScreen('schedule')}>Create Schedule</button>
