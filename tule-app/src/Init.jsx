fd180d527cc22f33dc388177516a2200459f2feb
WilliamVoker <williamvoker777@William-Vokers-Computer.local>
2023-03-22 23:49:40 -0400

    making pages look pretty

--- tule-app/src/Init.jsx
+++ tule-app/src/Init.jsx
@@ -7,22 +7,23 @@ import Col from "react-bootstrap/Col";
 const Init = (props) => {
 
     return (
-        
         <div>
             {/*<img src = {logo} style={{ width: 60, height: 60 }}></img>
-            <h1>Tule</h1>*/}
-            <Navbar text="Tule"/>
-            
-            <Form>
-                <Col>
-                    <Button onClick={() => props.onChangeScreen('login')}>
-                        Login
-                    </Button>
-                    <Button onClick={() => props.onChangeScreen('signup')}>
-                        Sign up
-                    </Button>
-                </Col>
-            </Form>
+            <h1>Tule</h1>*/}           
+
+               <Navbar text="Tule"/>
+               <div className='container'>
+                    
+                    <div className='v-align'>
+                        <Navbar text="Tule"/>
+                        <Button onClick={() => props.onChangeScreen('login')}>
+                            Login
+                        </Button>
+                        <Button onClick={() => props.onChangeScreen('signup')}>
+                            Sign up
+                        </Button>
+                    </div>
+                </div> 
             {/* <button onClick={() => props.onChangeScreen('login')}>Login</button>
             <button onClick={() => props.onChangeScreen('signup')}>Sign up</button> */}
         </div>  
