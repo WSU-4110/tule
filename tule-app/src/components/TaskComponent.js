6f22516fb45ed0536595824e907d30ffd530c0ab
WilliamVoker <williamvoker@gmail.com>
2023-04-18 23:16:48 -0400

    getting the latest changes

--- tule-app/src/components/TaskComponent.js
+++ tule-app/src/components/TaskComponent.js
@@ -2,20 +2,29 @@ import { EditTaskModal } from "../EditTaskModal";
 import { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import editIcon from '../pics/icons8-edit-64.png';
+import checkIcon from '../pics/icons8-checkmark-40.png';
 import { UilPen } from '@iconscout/react-unicons'
+import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
+
 function TaskComponent(props) {
    const [showModal, setShowModal] = useState(false);
 
     return(
         <>
             {showModal && <EditTaskModal key={props._id} editTask={props.editTask} resetModal={setShowModal} currentTasks={props.currentTasks} update={props.update}/>}
-                <div className="">
-                    <div className="mb-2 mt-5">
-                        <div className="">
-                            <img className="float-right" src={editIcon} alt=""/>   
-                            <h3>{props.task.Name}</h3>
+                <div className="mb-4 ">
+                    <div className="center">
+                        <div className="flex">
+                            {/*<img style={{width:30, height: 30}} src={checkIcon} alt="check-mark icon"/>
+                            <img style={{width:30, height: 30}} className="float-right" src={editIcon} alt=""/>   
+    <h3 className="pl-3">{props.task.Name}</h3>*/}
+                            <img style={{width:30, height: 30}} src={checkIcon} alt="check-mark icon"/>
+                            
+                            <img style={{width:30, height: 30}} className="float-right" src={editIcon} alt=""/>   
+                            
+                            <h3 className="pl-3">{props.task.Name}</h3>
                         </div>
-                        <Button className='' type = 'reset' onClick={() => setShowModal(true)}>Edit</Button>
+                        {/*<Button className='' type = 'reset' onClick={() => setShowModal(true)}>Edit</Button>*/}
                     </div>
                 </div>
 
