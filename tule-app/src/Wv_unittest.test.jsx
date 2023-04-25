ec593ba3d1200ec583e0d01f91eb03fdc81f9141
WilliamVoker <williamvoker@gmail.com>
2023-04-24 22:02:33 -0400

    fixing unit tests

--- tule-app/src/Wv_unittest.test.jsx
+++ tule-app/src/Wv_unittest.test.jsx
@@ -16,25 +16,25 @@ test('Edit button on the task page' , () => {
     expect(test).toBeInTheDocument();
 })
 
-test('Progress tracker' , () => {
+test('Progress tracker as a whole' , () => {
     render(<ProgressTracker ActiveTasks={[]}/>);
     var test = screen.getByTestId('progress');
     expect(test).toBeInTheDocument();
 })
 
-test('Task Name Rendering' , () => {
+test('Task Name Rendering in task list page' , () => {
     render(<Task task={[{Name: "", Complete:true}]}/>);
     var test = screen.getByTestId('name');
     expect(test).toBeInTheDocument();
 })
 
-test('Inital Pages login button' , () => {
+test('login button on inital Page' , () => {
     render(<Init/>);
     var test = screen.getByTestId('login-btn');
     expect(test).toBeInTheDocument();
 })
 
-test('Initial Pages Signup button' , () => {
+test('Signup button on initial Pages' , () => {
     render(<Init/>);
     var test = screen.getByTestId('signup-btn');
     expect(test).toBeInTheDocument();
