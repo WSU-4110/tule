9f4b8aae9d397b7da6f48591d9e711ad347450aa
WilliamVoker <williamvoker@gmail.com>
2023-04-24 20:46:05 -0400

    adding unit test

--- tule-app/src/components/TaskComponent.js
+++ tule-app/src/components/TaskComponent.js
@@ -25,9 +25,9 @@ function TaskComponent(props) {
                     <div className="container">
                         <div className="row align-items-start">
                             <div className="col-md-6 offset-md-5">
-                                <img style={{width:30, height: 30}} className="float-start" src={complete ? checkIconDark : checkIconLight}/>
-                                <img title="Edit Task" onClick={() => setShowModal(true)} className="float-start task-icon" style={{width:30, height: 30}} src={editIcon} alt="edit-pencil icon"/>
-                                <h3 className="float-start">{props.task.Name}</h3>
+                                <img data-testid="checkmark" style={{width:30, height: 30}} className="float-start" src={complete ? checkIconDark : checkIconLight}/>
+                                <img data-testid="edit-btn" title="Edit Task" onClick={() => setShowModal(true)} className="float-start task-icon" style={{width:30, height: 30}} src={editIcon} alt="edit-pencil icon"/>
+                                <h3 data-testid="name" className="float-start">{props.task.Name}</h3>
                                 <br/>
                             </div>
                         </div>
